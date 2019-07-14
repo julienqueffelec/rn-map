544e39dbeb24636f5e11f16917e79f764fb4c5d6
Julien Queffelec <julienqueffelec@macbook-pro-de-julien.home>
2019-07-14 23:13:27 +0200

    init commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <MapView.Marker
+            coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+            title={"Place de parking"}
+            description={"Utilisateur anonyme"}
+          />
+          <MapView.Marker
+            coordinate={{ latitude: 37.77825, longitude: -122.4324 }}
+            title={"Place de parking"}
+            description={"Utilisateur anonyme"}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
